d6c9d8440521967ee83f786f5792176416477a29
Basirat Hamayoon <basirathumayun@gmail.com>
2024-09-19 14:14:13 +0500

    Add some icons to the icons folder and also add close icon to menue button

--- script.js
+++ script.js
@@ -2,3 +2,7 @@ function showSlideBar(){
     var slideBar = document.querySelector('.slide-bar');
     slideBar.style.visibility = 'visible';
 }
+function hideSlideBar(){
+    var hideBar = document.querySelector('.slide-bar');
+    hideBar.style.visibility = 'hidden';
+}
